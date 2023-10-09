8730beab8d8be1ea863c266d674c8f7e3d16ab59
Valarie <Valarierono97@gmail.com>
2023-10-09 11:39:51 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/weatherapp/screen/navigation/BottomItem.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.screen.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomItem(var title:String, var icon: ImageVector, var screen_route:String){
+
+    object Home : BottomItem("Home", Icons.Default.Home,"home")
+    object Details : BottomItem("Details", Icons.Default.List,"details")
+}
